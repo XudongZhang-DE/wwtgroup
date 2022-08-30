89608ac024737d411a428cc7992d6738ddc33e6f
root <root@h2983032.stratoserver.net>
2022-08-30 16:07:34 +0200

    first version

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WWTGROUP</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"> WWTGROUP
+    </a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="#Profile of WWT" class="w3-bar-item w3-button">Profile of WWT</a>
+      <a href="#factory" class="w3-bar-item w3-button">Factory</a>
+      <a href="#about" class="w3-bar-item w3-button">About</a>
+      <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+      <a href="#location" class="w3-bar-item w3-button">Location</a>
+    </div>
+  </div>
+</div>
+
+<!-- darstellung Section -->
+<div class="w3-container w3-padding-64" id="Profile of WWT">
+</br>
+  <h1 class="w3-border-bottom w3-border-light-grey w3-padding-16">Profile of WWT</h1>
+<h4>WWT International Exhibition, specialized in exhibition engineering, is a seasoned professional multinational company. WWT is committed to turning the exhibition space a window to present the image of enterprises and their brands. By virtue of its original "WWT micro-innovation system", WWT serves clients with the concept of “Perfect Partner in Whole-journey” throughout the service cycle, providing one-stop integrated solutions for global exhibition and display, event creative planning and brand marketing promotion.
+</h4>
+</div>
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="w3-image" src="./darstellung/bg.JPG" alt="Architecture" width="1500" height="800">
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b></b></span> <span class="w3-hide-small w3-text-light-grey"></span></h1>
+  </div>
+</header>
+
+
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+  <!-- Project Section -->
+  <div class="w3-container w3-padding-32" id="projects">
+    <h1 class="w3-border-bottom w3-border-light-grey w3-padding-16">Projects</h1>
+    <h4>
+      WWT has 8 independent subsidiaries, production sites and storage bases in Germany, the United S
+      tates and China, close business partners from more than 80 countries and regions around the world, as
+       well as over 200 talents in exhibition design, which, coupled with its strong customer service team an
+       d engineering support team, make WWT standing out from the competitors. WWT’s one-stop global exhibition
+        solution covers from strategic positioning to market development, from creativity selection to 3D design
+        , from exhibits transportation to overseas storage, from booth construction to on-site
+       management. WWT not only build your booth, we also increase your brand value!
+    </h4>
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">SCC</div>
+        <img src="./darstellung/1.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Koshin</div>
+        <img src="./darstellung/2.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">LUXSHAREICT</div>
+        <img src="./darstellung/3.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Tianli</div>
+        <img src="./darstellung/4.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">SUPPO</div>
+        <img src="./darstellung/5.JPG" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Foodchem</div>
+        <img src="./darstellung/6.JPG" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Basic</div>
+        <img src="./darstellung/7.JPG" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Ningbo Herb</div>
+        <img src="./darstellung/8.JPG" alt="House" style="width:99%">
+      </div>
+    </div>
+
+    <div class="w3-row-padding">
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">RiGOL</div>
+          <img src="./darstellung/9.JPG" alt="House" style="width:99%">
+        </div>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">Develo new life</div>
+          <img src="./darstellung/10.JPG" alt="House" style="width:99%">
+        </div>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">Snack bar</div>
+          <img src="./darstellung/11.JPG" alt="House" style="width:99%">
+        </div>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">INOSITOL</div>
+          <img src="./darstellung/12.JPG" alt="House" style="width:99%">
+        </div>
+      </div>
+  </div>
+
+
+   <!-- factory Section -->
+   <div class="w3-container w3-padding-32" id="factory">
+    <h1 class="w3-border-bottom w3-border-light-grey w3-padding-16">Factory</h1>
+    <h4>
+      WWT International Exhibition is good at deeply digging and refining unique image in the brand connotation of 
+      enterprise. By strictly referring to CI system, WWT may make clear positioning and planning for commercial sp
+      ace for clients, provides one-stop services, including space planning, design, implementation and operation, 
+      for enterprise show room, large-scale business centers, as well as museums, science and technology museums, p
+      lanning museums, memorial halls, theme exhibition halls, theme-type culture and art display cam
+      paigns. WWT is the go-to platform for the enterprises to present their brand image and corporate culture.
+    </h4>
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Equipment</div>
+        <img src="./factory/5.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Tools</div>
+        <img src="./factory/2.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Factory Panorama</div>
+        <img src="./factory/3.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Supply of goods</div>
+        <img src="./factory/4.JPG" alt="House" style="width:100%">
+      </div>
+    </div>
+  </div>
+
+
+
+  <!-- About Section -->
+  <div class="w3-container w3-padding-32" id="about">
+    <h1 class="w3-border-bottom w3-border-light-grey w3-padding-16">About</h1>
+    <h4>WWT International Exhibition well verses in the mode and strategy of event marketing. With its unique planning and creative solutions, international innovative design concepts and professional experience in large event on-site layout, WWT provide overall planning and operation solutions for large-scale conferences, new product launches, product promotions, corporate year-end parties, theme forums, etc.
+    <hr>
+      WWT International Exhibition has been striving to, is committed to and will make every effort to enhance the brand value of each and every client with international vision, professional perspective, differentiated positioning, simple and effective design concept, and "zero defect" engineering spirit!
+    <hr>
+      WWT, your reliable multinational & professional exhibition engineering provider!
+      
+    </h4>
+  </div>
+
+  <div class="w3-row-padding w3-grayscale">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="About_1.png" alt="John" style="width:100%">
+      <h3>John Doe</h3>
+      <p class="w3-opacity">CEO & Founder</p>
+      <p>Email: 1981960667@qq.om </p>
+      <p>TEL: 015203949952 </p>
+      <p>WhatsApp: 015203949952 </p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="About_1.png" alt="Jane" style="width:100%">
+      <h3>Jane Doe</h3>
+      <p class="w3-opacity">Architect & Designer</p>
+      <p>Email: 1981960667@qq.om </p>
+      <p>TEL: 015203949952 </p>
+      <p>WhatsApp: 015203949952 </p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="About_1.png" alt="Mike" style="width:100%">
+      <h3>Mike Ross</h3>
+      <p class="w3-opacity">Architect & Designer</p>
+      <p>Email: 1981960667@qq.om </p>
+      <p>TEL: 015203949952 </p>
+      <p>WhatsApp: 015203949952 </p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="About_1.png" alt="Dan" style="width:100%">
+      <h3>Dan Star</h3>
+      <p class="w3-opacity">Architect & Designer</p>
+      <p>Email: 1981960667@qq.om </p>
+      <p>TEL: 015203949952 </p>
+      <p>WhatsApp: 015203949952 </p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+  </div>
+
+  <!-- Contact Section -->
+  <div class="w3-container w3-padding-32" id="contact">
+    <h1 class="w3-border-bottom w3-border-light-grey w3-padding-16">Contact</h1>
+    <p>Lets get in touch and talk about your next project.</p>
+    <form action="/action_page.php" target="_blank">
+      <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment">
+      <button class="w3-button w3-black w3-section" type="submit">
+        <i class="fa fa-paper-plane"></i> SEND MESSAGE
+      </button>
+    </form>
+  </div>
+  
+
+<!-- factory Section -->
+<div class="w3-container w3-padding-32" id="location">
+<h1 class="w3-border-bottom w3-border-light-grey w3-padding-16">Location</h1>
+<!-- Image of location/map -->
+<div class="w3-container">
+  <img src="About_1.png" class="w3-image" style="width:100%">
+</div>
+</div>
+
+
+<!-- End page content -->
+</div>
+
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <p>Powered by <a href="https://wwtgroup.de" title="Schenk" target="_blank" class="w3-hover-text-green">https://wwtgroup.de</a></p>
+</footer>
+
+</body>
+</html>
